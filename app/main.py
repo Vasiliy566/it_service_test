2df1d8b85cfb7fdafcef06a3e70d91ff985ca38b
Vasily Isaev <vasilii.isaev@kronshtadt.ru>
2021-02-04 00:23:09 +0300

    add checks of indexes

--- app/main.py
+++ app/main.py
@@ -16,12 +16,14 @@ get_distance_args = {"city_start": int, "city_finish": int}
 
 @app.route('/get_distance', methods=['GET'])
 def get_distance():
+    # Проверка api-ключа
+    # TODO: сравнивать хеш вместо самого ключа
     if "X-Api-Key" not in request.args or not request.args["X-Api-Key"] == '123321':
         resp = jsonify({"message": "ERROR: Unauthorized"})
         resp.status_code = 401
         return resp
 
-    args = {}
+    args = {}  # {названия агрументов : отформатированные значения}
     for k, v in get_distance_args.items():
         if k not in request.args:
             return no_request_argument_provided_error(k)
@@ -32,7 +34,13 @@ def get_distance():
             return wrong_type_argument_provided(k, v)
 
     distances = reformat_adj_matrix(np.load(open("data/matrix_distance", "rb")))
-    distance, path = dijkstra(args["city_start"], args["city_finish"], distances)
+    try:
+        distance, path = dijkstra(args["city_start"], args["city_finish"], distances)
+    except Exception as e:
+        logger.error(e)
+        resp = jsonify({"body": str(e)})
+        resp.status_code = 422
+        return resp
     resp = jsonify({
         "body": {
             "path": path,
